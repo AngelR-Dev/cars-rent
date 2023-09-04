d86151d93a26931eb18458a89d6eeeeda4a52a08
AngelR-Dev <111716367+AngelR-Dev@users.noreply.github.com>
2023-09-03 20:16:27 -0600

    initial commit

---
+++ types/index.ts
@@ -0,0 +1,58 @@
+// aqui especificamos los props del boton
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
+}
+
+// aqui espeficamos los props del searchbar
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+// interface de la proxima interface
+export interface OptionProps {
+  title: string; 
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+// showMore
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
